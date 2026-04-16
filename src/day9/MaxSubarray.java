54e710e28596c2cd0f1987e3cb9e07c117a9c192
Turaj <h_turaj@yahoo.com>
2026-04-16 10:10:01 +0400

    next commit

--- src/day9/MaxSubarray.java
+++ src/day9/MaxSubarray.java
@@ -1,5 +1,21 @@
 package day9;
 
 public class MaxSubarray {
+    public static int maxSubArray(int [] nums){
+        int maxSum = nums[0];
+        int currSum = 0;
+        for (int i=0; i<nums.length; i++){
+            if (currSum<0){
+                currSum=0;
+            }
+            currSum=currSum+nums[i];
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
 
+    public static void main(String[] args) {
+        int [] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(arr));
+    }
 }
