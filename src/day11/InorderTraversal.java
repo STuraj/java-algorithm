dada666bc58c00cbf77fce5907647e014b7747e4
Turaj <h_turaj@yahoo.com>
2026-04-20 11:07:31 +0400

    next commit

---
+++ src/day11/InorderTraversal.java
@@ -0,0 +1,12 @@
+//package day11;
+//
+//public class InorderTraversal {
+//    public static void inorder(TreeNode root){
+//        if (root==null){
+//            return;
+//        }
+//        inorder(root, left);
+//
+//    }
+//}
+//}
