7ae8d9bca459b3449807059eca50ca3b5b17dae9
Turaj <h_turaj@yahoo.com>
2026-05-02 22:24:48 +0400

    Remove element

---
+++ src/day16/RemoveElement.java
@@ -0,0 +1,25 @@
+package day16;
+
+public class RemoveElement {
+    public static int removeElement(int [] nums, int val){
+        int k=0;
+        for (int i=0; i<nums.length; i++){
+            if (nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,2,3};
+        int val=3;
+        int result = removeElement(nums,val);
+        System.out.println("New length:" + result);
+        for (int i=0; i<result;i++){
+            System.out.print(nums[i] +" ");
+        }
+    }
+}
