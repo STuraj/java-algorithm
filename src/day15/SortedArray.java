8f591e819b636e07fff7ff7625a1cc003d6d608e
Turaj <h_turaj@yahoo.com>
2026-05-06 08:10:33 +0400

    Sorted Array

---
+++ src/day15/SortedArray.java
@@ -0,0 +1,11 @@
+package day15;
+
+import java.util.Arrays;
+
+public class SortedArray {
+    public static void main(String[] args) {
+        int [] arr = {4, 1, 3, 2, 5};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
